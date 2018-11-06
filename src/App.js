c0326152fff230c2b0a104e394d9be61ceeed9ac
Sandra Chen <scchen17@gmail.com>
2018-11-05 18:28:37 -0800

    filter list item

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
     Promise.all([googleMapsPromise, placesPromise]).then(values => {
       let google = values[0];
-      let venues = values[1].response.venues;
+      this.venues = values[1].response.venues;
 
       this.google = google;
       this.markers = [];
@@ -24,10 +24,13 @@ class App extends Component {
       this.map = new google.maps.Map(document.getElementById("map"), {
         zoom: 9,
         scrollwheel: true,
-        center: { lat: venues[0].location.lat, lng: venues[0].location.lng }
+        center: {
+          lat: this.venues[0].location.lat,
+          lng: this.venues[0].location.lng
+        }
       });
 
-      venues.forEach(venue => {
+      this.venues.forEach(venue => {
         let marker = new google.maps.Marker({
           position: { lat: venue.location.lat, lng: venue.location.lng },
           map: this.map,
@@ -57,16 +60,26 @@ class App extends Component {
 
         this.markers.push(marker);
       });
+      this.setState({ filteredVenues: this.venues });
     });
   }
-
+  listItemClick = venue => {
+    let marker = this.markers.filter(m => m.id === venue.id)[0];
+    this.infowindow.setContent(marker.name);
+    this.map.setCenter(marker.position);
+    this.infowindow.open(this.map, marker);
+    this.map.panBy(0, -125);
+  };
   filterVenues(query) {
+    let f = this.venues.filter(venue =>
+      venue.name.toLowerCase().includes(query.toLowerCase())
+    );
     this.markers.forEach(marker => {
       marker.name.toLowerCase().includes(query.toLowerCase()) === true
         ? marker.setVisible(true)
         : marker.setVisible(false);
     });
-    this.setState({ query });
+    this.setState({ filteredVenues: f, query });
   }
 
   render() {
@@ -75,11 +88,27 @@ class App extends Component {
         <div id="map" />
         <div id="sidebar">
           <input
+            placeholder="Filter content"
             value={this.state.query}
             onChange={e => {
               this.filterVenues(e.target.value);
             }}
           />
+          <br />
+          <br />
+          {this.state.filteredVenues &&
+            this.state.filteredVenues.length > 0 &&
+            this.state.filteredVenues.map((venue, index) => (
+              <div
+                className="venue-item"
+                key={venue.id}
+                onClick={() => {
+                  this.listItemClick(venue);
+                }}
+              >
+                {venue.name}
+              </div>
+            ))}
         </div>
       </div>
     );
