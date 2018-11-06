e824fe040ce22a1fadbdbd5e440ec46fb87edd0a
Sandra Chen <scchen17@gmail.com>
2018-11-05 17:40:08 -0800

    searchbar and infowindow

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import "./App.css";
 import { load_google_maps, load_places } from "./utils";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ""
+    };
+  }
   componentDidMount() {
     let googleMapsPromise = load_google_maps();
     let placesPromise = load_places();
@@ -30,15 +36,51 @@ class App extends Component {
           name: venue.name,
           animation: google.maps.Animation.DROP
         });
-        this.markers.push(this.marker);
+
+        marker.addListener("click", () => {
+          if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }
+          setTimeout(() => {
+            marker.setAnimation(null);
+          }, 1500);
+        });
+
+        google.maps.event.addListener(marker, "click", () => {
+          this.infowindow.setContent(marker.name);
+          this.map.setCenter(marker.position);
+          this.infowindow.open(this.map, marker);
+          this.map.panBy(0, -125);
+        });
+
+        this.markers.push(marker);
       });
     });
   }
+
+  filterVenues(query) {
+    this.markers.forEach(marker => {
+      marker.name.toLowerCase().includes(query.toLowerCase()) === true
+        ? marker.setVisible(true)
+        : marker.setVisible(false);
+    });
+    this.setState({ query });
+  }
+
   render() {
     return (
       <div className="app">
         <div id="map" />
-        <div id="sidebar" />
+        <div id="sidebar">
+          <input
+            value={this.state.query}
+            onChange={e => {
+              this.filterVenues(e.target.value);
+            }}
+          />
+        </div>
       </div>
     );
   }
